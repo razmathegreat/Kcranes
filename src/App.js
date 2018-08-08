936dd7e0bc3b3176518dc6696859e10b544212ac
Mike Flatley <flatley.art@gmail.com>
2018-08-07 21:20:46 -0500

    Updated app to have basic counter and static state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+const TotalCranes = (props) => {
+  return (
+    <h2>
+      {props.counter}
+    </h2>
+  )
+};
+
+class Button extends React.Component{
+  handleClick = () => {
+    this.props.onClickFunction();
+  }
+  render(){
+    return(
+      <button onClick={this.handleClick}>
+        I finished a crane!
+      </button>      
+    )
+  };
+};
+
 class App extends Component {
+  state = { counter:0 };
+
+  incrementCounter = () =>{
+    this.setState((prevState)=>({
+      counter : prevState.counter + 1}    
+    )
+  )
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <img src='http://worldartsme.com/images/paper-crane-clipart-1.jpg' className="App-logo"  />
+          <h1 className="App-title">1000 Paper Cranes</h1>
+        </header>        
+        <TotalCranes counter={this.state.counter} />
+        <Button onClickFunction={this.incrementCounter}/>
       </div>
     );
   }
